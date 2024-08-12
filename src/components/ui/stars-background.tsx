46829de0444305a8708094c76984741d0cf908f3
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-08-12 07:42:23 +0530

    removed star background

--- src/components/ui/stars-background.tsx
+++ src/components/ui/stars-background.tsx
@@ -26,7 +26,7 @@ interface StarBackgroundProps {
 }
 
 export const StarsBackground: React.FC<StarBackgroundProps> = ({
-  starDensity = 0.0004,
+  starDensity = 0.0,
   allStarsTwinkle = true,
   twinkleProbability = 0.7,
   minTwinkleSpeed = 0.3,
