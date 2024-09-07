1b5e467a2f2e6f5a942fdcf6cdff09bde95cd661
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-09-07 21:52:14 +0530

    fixed an issue

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,22 @@
+import { message } from "antd";
 import { FaDownload } from "react-icons/fa6";
 
 function Button() {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const info = () => {
+    messageApi.info("At the moment, Please contact me to receive my resume");
+  };
   return (
-    <div className="mt-4">
-      <div className="inline-flex justify-center items-center gap-2 py-4 px-8 rounded-lg hover:cursor-pointer bg-gradient-to-r from-[#139B43] to-[#139b43] hover:from-[#139B43] hover:to-[#31ad5d] ">
-        <FaDownload />
-        Download CV
+    <>
+      {contextHolder}
+      <div className="mt-4" onClick={info}>
+        <div className="inline-flex justify-center items-center gap-2 py-4 px-8 rounded-lg hover:cursor-pointer bg-gradient-to-r from-[#139B43] to-[#139b43] hover:from-[#139B43] hover:to-[#31ad5d] ">
+          <FaDownload />
+          Download CV
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
